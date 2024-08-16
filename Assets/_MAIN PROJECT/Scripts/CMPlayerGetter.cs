751b632e1cd8bc272a3e71fcf8ee6ac6b16aa448
Ale Insausti <alejandroninsausti@gmail.com>
2024-08-16 16:46:29 -0300

    Update Camera values

---
+++ Assets/_MAIN PROJECT/Scripts/CMPlayerGetter.cs
@@ -0,0 +1,34 @@
+using Cinemachine;
+using UnityEngine;
+
+namespace PlanetMover.Cam
+{
+    [RequireComponent(typeof(CinemachineVirtualCameraBase))]
+    public class CMPlayerGetter : MonoBehaviour
+    {
+        [Header("Set Values")]
+        [SerializeField] CinemachineVirtualCameraBase cam;
+        //[Header("Runtime Values")]
+
+        //Unity Events
+        void Awake()
+        {
+            if(!cam)
+                cam = GetComponent<CinemachineVirtualCameraBase>();
+            
+            if(cam.Follow) return; //camera already has a follow object
+
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+            if (!player)
+            {
+                Debug.LogError("No player found!!!");
+                return;
+            }
+            
+            cam.Follow = player.transform;
+        }
+
+        //Methods
+    }
+}
